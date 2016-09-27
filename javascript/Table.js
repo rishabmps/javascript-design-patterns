e8f541869e273a4f15d534b1a4352fec419d9f10
rishabmps <rishab.mps@gmail.com>
2016-09-27 14:22:28 +0530

    table Modifier using module pattern

--- javascript/Table.js
+++ javascript/Table.js
@@ -2,26 +2,38 @@
  * Created by idnrsj on 9/26/2016.
  */
 
-var rows = {};
-var index = 0;
 
-function insertRow() {
+
+class data{
+
+    constructor(rows,index){
+        this.rows = rows;
+        this.index = index;
+    }
+
+
+
+    insertRow() {
     var table = document.getElementById("myTable")
-    rows[index] =  table.insertRow(index);
-    var row = rows[index];
+    this.rows[this.index] =  table.insertRow(this.index);
+    var row = this.rows[this.index];
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
     cell1.innerHTML =   '<input type="text" class="fname" />';
-    cell2.innerHTML = '<input type="button" value="Delete" onclick="deleteRow(this)" />';
+    cell2.innerHTML = '<input type="button" value="Delete" onclick="data_table.deleteRow(this)" />';
 
-    index++;
-    console.log("hi");
-}
+    this.index++;
 
-function deleteRow(row){
+    }
+
+    deleteRow(row){
     var table = document.getElementById("myTable")
     var  i = row.parentNode.parentNode.rowIndex;
     table.deleteRow(i);
-    delete rows[i];
-    index--;
+    delete this.rows[i];
+    this.index--;
+    }
+
 }
+
+let data_table = new data({},0);
